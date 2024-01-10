3ae0def5ddfae5ff66ba05a28e6ac5ea5c759635
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-10 22:29:04 +0600

    contries

---
+++ src/components/Countries/Contries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contries = () => {
+    return (
+        <div>
+            <h2>Contries in other file.</h2>
+        </div>
+    );
+};
+
+export default Contries;
