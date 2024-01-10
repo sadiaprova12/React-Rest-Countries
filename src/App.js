505df75292a93878bd4e71b1c7819be659d6b727
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-10 18:57:17 +0600

    load countries using use effect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
@@ -11,9 +12,25 @@ function App() {
 
 // make a component
 function LoadCountries(){
+  /**
+   1. Declaring State to save loaded data
+   */
+  const [countries, setCountries] = useState([]);
+  /** 
+   2. Writing useEffect()
+  */
+
+   useEffect( () =>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res=>res.json())
+    // .then(data => console.log(data))
+    .then(data => setCountries(data))
+   }, [])
+
   return(
     <div>
       <h1>Visiting Every Country Of The World!!!</h1>
+      <h3>Available Countries: {countries.length}</h3>
     </div>
   )
 }
