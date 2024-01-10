47833cbc18bc2a3f74af9015622f57c6d3212ee3
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-10 22:28:16 +0600

    Contries components

--- src/App.js
+++ src/App.js
@@ -1,55 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Contries from './components/Countries/Contries';
 
 function App() {
   return (
     <div className="App">
-     <LoadCountries></LoadCountries>
+     
+     <Contries></Contries>
     </div>
   );
 }
 
-// make a component
-function LoadCountries(){
+
+/* function LoadCountries(){
   /**
    1. Declaring State to save loaded data
    */
-  const [countries, setCountries] = useState([]);
+  /* const [countries, setCountries] = useState([]);
   /** 
    2. Writing useEffect()
   */
 
-   useEffect( () =>{
+   /*useEffect( () =>{
     fetch('https://restcountries.com/v3.1/all')
     .then(res=>res.json())
     // .then(data => console.log(data))
     .then(data => setCountries(data))
    }, [])
 
-  return(
+ /* return(
     <div>
       <h1>Visiting Every Country Of The World!!!</h1>
       <h3>Available Countries: {countries.length}</h3>
       {
         // countries.map(country => console.log(country))
         // countries.map(country => <p{country.name.common}></p>)
-        countries.map(country => <Country name={country.name.common}></Country>)
+        /*countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
       }
     </div>
   )
 }
 
 /**
+ Display Countries In A Simple Way, Folder Structure
+ */
+
+/**
  Eka dhik component
  */
 
- function Country(props){
+ /*function Country(props){
   return (
     <div>
       <h2>Name: {props.name}</h2>
+      <h4>Population: {props.population}</h4>
     </div>
   )
- }
+ }*/
 
 export default App;
