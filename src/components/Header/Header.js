629f3cab02cae8ab85a83f6760cc7470a98c585f
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-12 20:25:12 +0600

    nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import './Header.css'
 const Header = () => {
     return (
         <div>
             <h2>Welcome to my React Website</h2>
+            <nav className='menu'>
+                <a href="/home">Home</a>
+                <a href="/shop">Shop</a>
+                <a href="/about">About Us</a>
+            </nav>
         </div>
     );
 };
